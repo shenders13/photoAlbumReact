9cb04792678f646b910b7437b73ff68a7610cda7
Sam Henderson <hendersonsam456@gmail.com>
2016-10-06 21:08:30 -0700

    changes to photo albulm back bone

---
+++ photoAlbulmBackBone/models/AppModel.js
@@ -0,0 +1,11 @@
+var AppModel = Backbone.Model.extend({
+  initialize: function(params){
+    this.set('currentImage', new ImageModel());
+
+    // params refers to an instance of ImageCollection
+    params.photocollection.on('[INSERT COLLECTION METHOD]', function(image) {
+      this.set('currentImage', song)
+    }, this);
+
+  }
+});
