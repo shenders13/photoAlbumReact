d497a4756518b3d662965430295c93e87c2c4615
Sam Henderson <hendersonsam456@gmail.com>
2016-10-17 17:29:52 -0700

    express server successfully serving assets and data

---
+++ react-photo-app/server/server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+
+var imageData = [
+  {
+    id: 0,
+    url: 'http://www.freedigitalphotos.net/images/img/homepage/87357.jpg',
+    title: 'Duck',
+    rating: 3
+  },
+  {
+    id: 1,
+    url: 'http://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg',
+    title: 'Jellyfish',
+    rating: 4
+  },
+  {
+    id: 2,
+    url: 'http://www.spyderonlines.com/images/wallpapers/images/images-22.jpg',
+    title: 'Bear',
+    rating: 1
+  },
+  {
+    id: 3,
+    url: 'http://eskipaper.com/images/images-4.jpg',
+    title: 'Dog',
+    rating: 5
+  },
+  {
+    id: 4,
+    url: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/P.t.altaica_Tomak_Male.jpg',
+    title: 'Tiger',
+    rating: 5
+  }
+];
+
+app.use('/', express.static('../client'));
+
+app.get('/images', function (req, res) {
+  console.log('received get request');
+  res.send(imageData);
+});
+
+app.listen(8080, function () {
+  console.log('Photo Outlet Server listening on port 8080!');
+});
